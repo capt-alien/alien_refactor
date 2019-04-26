15a5f14554e489dc35dc86f5063fecacd5df0917
capt-alien <eric.botcher@presidio.edu>
2019-04-26 16:15:51 -0700

    string formating

--- spaceman.py
+++ spaceman.py
@@ -19,41 +19,57 @@ def is_word_guessed(secret_word, letters_guessed):
             return True
     return False
 
-def is_given_guess_correct(guessed_letter, secret_word):
+def guess_correct(guessed_letter, secret_word):
     """Returns boolian if the guessed letter is correct"""
     if guessed_letter in secret_word:
         return True
     else:
         return False
 
-def get_guessed_word(secret_word, letters_guessed):
+def g_word(secret_word, letters_guessed):
+    """returns word if letters are all guessed"""
     word = ""  #Declare a variable with an empty string
-    for letter in secret_word:  #Starts a for loop for letters get_guessed_word
+    for letter in secret_word:  #Starts a for loop for letters g_word
         if letter in letters_guessed:
             word += letter + ""  # Could I rewrite this with .append?
     else:
         word += "_"
     return word
 
+
+intro = [
+    "Welcome to the Spaceman's Airlock",
+    "I come in peace, GET ME OUT OF THIS AIRLOCK!",
+    "Please Help me hack the passcode, by guessing the letters:",
+    "The passcode has {} charictors ",
+    "You have 7 guesses before the airlock automatically opens.",
+    "Guess a Letter HotShot ",
+    "So far, you have guessed:{}",
+    "Revealed Password: {}",
+]
+
+
+
+
 def spaceman(secret_word):
     """Runs Program and calls all the other functions"""
     secret_word_ln = len(secret_word)  #saves lenght of string to objects
     letters_guessed = []
     count = 8
-    print("Welcome to the Spaceman's Airlock")
-    print("I come in peace, GET ME OUT OF THIS AIRLOCK!")
-    print("Please Help me hack the passcode, by guessing the letters: ")
-    print("The passcode has %s charictors " % secret_word_ln)
-    print("You have 7 guesses before the airlock automatically opens.")
+    print(intro[0])
+    print(intro[1])
+    print(intro[2])
+    print(intro[3].format(secret_word_ln))
+    print(intro[4])
 
     while is_word_guessed(secret_word, letters_guessed) and count > 0:
-        guessed_letter = input("Guess a Letter HotShot ")
+        guessed_letter = input(intro[5])
         if guessed_letter not in letters_guessed:
             letters_guessed.append(guessed_letter)
-            print("So far, you have guessed:{}".format(letters_guessed)) #shows word with underscores for tcorrect letters in order
-            print("Revealed Password: {}".format(get_guessed_word(secret_word, letters_guessed)))
+            print(intro[6].format(letters_guessed)) #shows word with underscores for tcorrect letters in order
+            print(intro[7].format(g_word(secret_word, letters_guessed)))
             #Create if statement to determine if letters are correctly
-            if is_given_guess_correct(guessed_letter, secret_word):
+            if guess_correct(guessed_letter, secret_word):
                 print("Correct!")
                 life(count, secret_word)
                 print(f"\nAirlock Countdown: {count}")
